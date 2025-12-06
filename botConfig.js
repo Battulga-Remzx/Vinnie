9c441a0da0382131c90dcc63e2609eea0fb20cff
Battulga-Remzx <89357725+Battulga-Remzx@users.noreply.github.com>
2025-12-07 05:52:12 +0800

    Update botConfig.js with actual token
    Replaced token placeholder with actual token value.

--- botConfig.js
+++ botConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: process.env.token,
+  token: 'MTQ0NjkzMjM0ODMyMjA1ODI5MA.GZEgzS.beD-OI-aHa1gL9oPvrPuN7LmAJEDUAvnO7diXY',
   prefix: ".",
   admins: [
     "PEOPLE WHO CAN USE ADD MONEY (IDS)"
